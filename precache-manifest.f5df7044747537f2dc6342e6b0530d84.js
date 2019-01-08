e45f9c928c5a377a6aa5f50d018fdbac223b1b7e
akulah91 <akulah91@gmail.com>
2019-01-08 20:37:29 +0800

    Updates

---
+++ precache-manifest.f5df7044747537f2dc6342e6b0530d84.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e8cef0013c2427dac628adf30a2792be",
+    "url": "/reactresume/static/media/rahmatNew.e8cef001.jpg"
+  },
+  {
+    "revision": "eec687aee9afbb26905f09c15767eb62",
+    "url": "/reactresume/static/media/hrmpro.eec687ae.jpeg"
+  },
+  {
+    "revision": "1475608893ca4bd7c1ed0a0a5a24d255",
+    "url": "/reactresume/static/media/attnd.14756088.jpg"
+  },
+  {
+    "revision": "6e4733ee1668d65f7f369f3994d9bffd",
+    "url": "/reactresume/static/media/QuikHalalFull.6e4733ee.png"
+  },
+  {
+    "revision": "16c044ab95f399893093",
+    "url": "/reactresume/static/js/runtime~main.16c044ab.js"
+  },
+  {
+    "revision": "2b68715c94e57e95c56f",
+    "url": "/reactresume/static/js/main.2b68715c.chunk.js"
+  },
+  {
+    "revision": "c9376c7fc677f1f3e74c",
+    "url": "/reactresume/static/js/1.c9376c7f.chunk.js"
+  },
+  {
+    "revision": "2b68715c94e57e95c56f",
+    "url": "/reactresume/static/css/main.62eaef57.chunk.css"
+  },
+  {
+    "revision": "2d24008eae75b159cc6f30dcf4443367",
+    "url": "/reactresume/index.html"
+  }
+];
